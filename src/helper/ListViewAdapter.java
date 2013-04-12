9e9341a7a79ca6df88ad2fabe055b9941f6ab83b
Mohammad Fajarrizky Abidin <mohammad.fajarrizky@gmail.com>
2013-04-12 19:23:09 +0700

    listView
    listview editing

--- src/helper/ListViewAdapter.java
+++ src/helper/ListViewAdapter.java
@@ -1,22 +1,50 @@
 package helper;
 
+import java.util.ArrayList;
+
+import model.Tarian;
+
+import android.R;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 
-public class ListViewAdapter extends ArrayAdapter<Object> {
+public class ListViewAdapter extends BaseAdapter {
 
-	public ListViewAdapter(Context context, int textViewResourceId,
-			Object[] objects) {
-		super(context, textViewResourceId, objects);
-		// TODO Auto-generated constructor stub
-	}
+	private Tarian[] tarianArray;
+	private LayoutInflater mInflater;
 	
-	@Override
+	public ListViewAdapter (Context context, Tarian[] results){
+		tarianArray = results;
+		mInflater = LayoutInflater.from(context);
+	}
+
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return tarianArray.length;
+	}
+
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return tarianArray[arg0];
+	}
+
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return super.getView(position, convertView, parent);
+		if (convertView == null){
+			convertView = mInflater.inflate(R.layout.simple_list_item_1, null);
+			
+		}
+		
+		return null;
 	}
 	
 	
