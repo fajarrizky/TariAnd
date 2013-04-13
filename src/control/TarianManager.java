95902e9772b439a11ecc0dd15dbc6bfc9a2d7b14
Rinaldi Akbar Setiawan <rinaldiakbar.setiawan@gmail.com>
2013-04-13 17:57:29 +0700

    Coba Konek
    afsef

--- src/control/TarianManager.java
+++ src/control/TarianManager.java
@@ -2,7 +2,12 @@ package control;
 
 import java.util.ArrayList;
 import java.util.ListIterator;
-
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.util.Log;
+import control.CustomHttpClient;
 import model.Tarian;
 
 public class TarianManager {
@@ -14,10 +19,40 @@ public class TarianManager {
 
 	//niatnya ini buat ngeretrieve dari database, masukin semua tarian ke list.
 	public void retrieve(){
-
-	}
-
-	
+		ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();  
+		//postParameters.add(new BasicNameValuePair("Nama", input.getText().toString()));
+		String response = null;
+		try {
+			response = CustomHttpClient.executeHttpPost("http://192.168.0.105/android/daftar_tarian.php", postParameters);
+		    String result = response.toString();  
+		    try{
+		    	JSONArray jArray = new JSONArray(result);
+		        int [] ID = new int [jArray.length()];
+		        String [] Nama = new String [jArray.length()];
+		        String [] Provinsi = new String [jArray.length()];
+		        String [] Deskripsi = new String [jArray.length()];
+		        
+		        for(int i=0;i<jArray.length();i++){
+		        	JSONObject json_data = jArray.getJSONObject(i);
+			        ID[i] = Integer.parseInt(json_data.getString("ID"));
+			        Nama[i] = json_data.getString("Nama");
+		        	Provinsi[i] = json_data.getString("Provinsi");	         
+			        Deskripsi[i] = json_data.getString("Deskripsi");
+			        Tarian tarian = new Tarian(ID[i], Nama[i]);
+			        tarian.setDescription(Deskripsi[i]);
+			        tarian.setLocation(Provinsi[i]);
+			        listTarian.add(tarian);
+			        }
+		    } 
+		    catch(JSONException e){
+		    	Log.e("log_tag", "Error parsing data "+e.toString());
+		    }
+		    }
+		catch (Exception e) {
+			Log.e("log_tag","Error in http connection!!" + e.toString());     
+		}
+	}         
+		
 	//yang ini buat nyari dari nama
 	public ArrayList<Tarian> searchByName(String targetName){
 		ArrayList<Tarian> target = new ArrayList<Tarian>();
